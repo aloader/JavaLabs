57e3647f30ac20fc161e8cd3aef6f83625e8493f
dkat <kda@tut.by>
2019-05-29 15:47:49 +0300

    1. Task "annotation" Run configuration "AnnotationDemo"
    2. Task "lambdas"
    Run configuration "LambdaDemo"

--- src/StreamsDemo3.java
+++ src/StreamsDemo3.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import streams3.*;
 
 public class StreamsDemo3 {
+    public static void main(String[] args) {
+//        new task01_FindFirstObject();
+//        new task02_FindAnyObject();
+//        new task03_WordsFromFileByFrequency();
+//        new task04_StreamToArrayList();
+//        new task05_LenghtWordsMoreThan10();
+//        new task06_HashMap10MostFreqWords();
+//        new task07_HashMap10MostProbWords();
+//        new task08_NumberOfElementsInArray();
+//        new task09_ObjectsQuantityWithDateEqualCurrentYear();
+//        new task10_IsUserAdminExist();
+//        new task11_IsObjectNotCurrentYearExist();
+//        new task12_MinNumberInArray();
+//        new task13_MaxNumberInArray();
+//        new task14_NearestPoint();
+//        new task15_FarthestPoint();
+//        new task16_ExponentalDistribution();
+//        new task17();
+//        new task22_IntStreamFromStringArray();
+//        new task23_SumOddNumbers();
+//        new task24_SplitArrayOfObjectsByLogged();
+        new task25();
+    }
 }
